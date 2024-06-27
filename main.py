77315edf613655cb53cd1e6404932486beb74822
JW <walnikjm@gmail.com>
2024-06-27 13:57:44 +0200

    Add more descriptive code

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import os
 import sys
 import ffmpeg
 
-def download_yt_music(url):
+def download_yt_music(url: str) -> str:
+    """
+    Download music directly from the YT portal using url
+    """
     output_dir="./music"
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
@@ -12,7 +15,10 @@ def download_yt_music(url):
     output_file=audio_stream.download(output_path=output_dir)
     return output_file
 
-def change_frequency_to_432(output_file):
+def change_frequency_to_432(output_file: str) -> str:
+    """
+    Change frequency of the input file and save it to the directory
+    """
     filename = f"{os.path.splitext(output_file)[0]}-432Hz.mp3"
     filter_chain = 'asetrate=44100*432/440,atempo=440/432,aresample=44100'
     if os.path.exists(filename):
@@ -29,7 +35,10 @@ def change_frequency_to_432(output_file):
     print(f"The audio is downloaded as {filename}")
     return filename
     
-def check_freq_of_mp3(file_path):
+def check_freq_of_mp3(file_path: str) -> None:
+    """
+    Check if the process succede and music has been changed to 432Hz
+    """
     probe = ffmpeg.probe(file_path)
     audio_streams = [stream for stream in probe['streams'] if stream['codec_type'] == 'audio']
     
